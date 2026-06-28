ad6a06762d7ae687affb7caf9cb8f90b69f1fe8b
Noah Lovelady-Allen <noah@loveladyallen.com>
2026-06-27 21:53:10 -0400

    add .meta.js file

---
+++ contributor-assistant-plus.meta.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Contributor Assistant+
+// @namespace    http://tampermonkey.net/
+// @version      2026-06-27g
+// @description  Marks contributors on stock sites as American or foreign, and adds country filters to Pond5 and Envato.
+// @author       You
+// @match        https://*.shutterstock.com/video/*
+// @match        https://*.shutterstock.com/image-photo/*
+// @match        https://*.shutterstock.com/image-vector/*
+// @match        https://*.pond5.com/stock-footage/item/*
+// @match        https://*.pond5.com/stock-images/photos/item/*
+// @match        https://stock.adobe.com/video/*
+// @match        https://stock.adobe.com/images/*
+// @match        https://elements.envato.com/*
+// @exclude      https://elements.envato.com
+// @match        https://www.storyblocks.com/video/stock/*
+// @match        https://artgrid.io/clip/*
+// @match        https://artlist.io/stock-footage/clip/*
+// @match        https://dissolve.com/video/*
+// @match        https://dissolve.com/stock-photo/*
+// @match        https://www.istockphoto.com/video/*
+// @match        https://www.istockphoto.com/photo/*
+// @match        https://www.pond5.com/search?*
+// @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_registerMenuCommand
+// @grant        window.onurlchange
+// ==/UserScript==
